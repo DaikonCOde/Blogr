eb7d2dab2cc248f86266a6a95fb04877c8fb9aac
Alex Ocsa <daikon.code@gmail.com>
2021-06-02 18:28:22 -0500

    efecto animado

---
+++ js/observer.js
@@ -0,0 +1,22 @@
+const elements = [...document.querySelectorAll(".animation")];
+
+let options = {
+  threshold: 0.25
+}
+
+function ObserverObjec (element) {
+  console.log(element)
+  element.forEach(e => {
+    let target = e.target;
+    if(e.isIntersecting) {
+      let classAnimation = target.dataset.animation;
+      target.classList.add(classAnimation)
+      observer.unobserve(target);
+    }
+  })
+}
+
+const observer = new IntersectionObserver(ObserverObjec, options);
+elements.forEach(element => {
+  observer.observe(element);
+})
